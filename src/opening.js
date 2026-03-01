73e4ee4cc3d71acb521ae3c46f60f723b2b528f9
Raaina <raaina.n.daniar@gmail.com>
2026-03-01 11:37:21 -0500

    animation attempt, errors exist

--- src/opening.js
+++ src/opening.js
@@ -6,15 +6,31 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
+        this.load.spritesheet('player','assets/Flyingduck.png',{
+        frameWidth: 64,// Width of one frame
+        frameHeight: 64, // Height of one frame
+});
     }
 
     create() {
         this.cameras.main.setBackgroundColor("#904242");
-    }
 
-    update() {
+        let frames = [];
+
+        for (let i = 0; i < 6; i++) {  // change 6 to however many rows you have
+            frames.push(i * 2);
+        }
+
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNumbers('player', { frames: frames }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        const player = this.add.sprite(100, 100, 'player');
+        player.play('walk');
     }
-}
 
 const config = {
     type: Phaser.AUTO,
