dd666073f4a38dd12f0d0f207f3ac0c38a6937a9
Raaina <raaina.n.daniar@gmail.com>
2026-03-01 11:57:22 -0500

    reworked back to background

--- src/opening.js
+++ src/opening.js
@@ -6,31 +6,15 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.spritesheet('player','assets/Flyingduck.png',{
-        frameWidth: 64,// Width of one frame
-        frameHeight: 64, // Height of one frame
-});
     }
 
     create() {
         this.cameras.main.setBackgroundColor("#904242");
-
-        let frames = [];
-
-        for (let i = 0; i < 6; i++) {  // change 6 to however many rows you have
-            frames.push(i * 2);
-        }
-
-        this.anims.create({
-            key: 'walk',
-            frames: this.anims.generateFrameNumbers('player', { frames: frames }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        const player = this.add.sprite(100, 100, 'player');
-        player.play('walk');
     }
+
+    update() {
+    }
+}
 
 const config = {
     type: Phaser.AUTO,
@@ -41,3 +25,4 @@ const config = {
 };
 
 new Phaser.Game(config);
+
