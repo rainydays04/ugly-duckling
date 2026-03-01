5c300d3e60406b1f1a7e07a7b789d6d296926050
Raaina <raaina.n.daniar@gmail.com>
2026-03-01 11:19:40 -0500

    added packages and test background

--- src/opening.js
+++ src/opening.js
@@ -1,11 +1,27 @@
-import Phaser from "phaser"
+import Phaser from "phaser";
+
 class GameScene extends Phaser.Scene {
     constructor() {
-		super("GameScene")
-	}
-    preload(){
-        this.preload.spritesheet("goose","name for goose file"{
-            //height and size stuff
-        });
+        super("GameScene");
+    }
+
+    preload() {
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor("#904242");
+    }
+
+    update() {
     }
 }
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: "game",
+    scene: [GameScene]
+};
+
+new Phaser.Game(config);
